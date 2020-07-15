2b78739540351e9936254271ff6b9be054ac971b
petarPAN <petarburic554@gmail.com>
2020-07-15 23:24:57 +0200

    Requiring passport packages

--- v1/models/user.js
+++ v1/models/user.js
@@ -1,8 +1,11 @@
 var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
     username: String,
     password: String
 });
 
-module.exports = mongoose.model("User", userSchema)
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
